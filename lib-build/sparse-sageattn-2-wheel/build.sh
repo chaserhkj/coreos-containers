bf0e8b98ea34cea41e4c06b651d25c327f003a91
Chaser Huang <huangkangjing@gmail.com>
2025-08-25 19:51:41 -0400

    sparse sage attn wheel build

--- lib-build/sparse-sageattn-2-wheel/build.sh
+++ lib-build/sparse-sageattn-2-wheel/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf vendor && mkdir vendor
 (cd vendor && git clone --depth=1 https://github.com/Radioheading/Block-Sparse-SageAttention-2.0)
-podman build . --target build -t sparse-sageattn-2-wheel
+podman build . -t sparse-sageattn-2-wheel
