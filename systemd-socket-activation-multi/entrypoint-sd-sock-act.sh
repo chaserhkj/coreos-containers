8903d59c8364586090649eac1bc0288afe786678
Chaser Huang <huangkangjing@gmail.com>
2025-09-12 00:36:36 -0400

    Systemd-socket-activation for multiple sockets

---
+++ systemd-socket-activation-multi/entrypoint-sd-sock-act.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+# Use sh for compatibility
+
+${SD_SOCK_ACT_BASE_CMD} &
+
+SRV_PID=$!
+clean() {
+    echo "[SD_SOCK_ACT] Shutting down service process..."
+    kill $SRV_PID
+    exit 0
+}
+trap "clean" EXIT
+
+sockets=($SD_SOCK_ACT_FWD_TARGETS)
+tunnel_pids=()
+sock_fd=3
+
+for sock in "${sockets[@]}"; do
+
+until socat /dev/null $sock >/dev/null 2>&1
+do sleep ${SD_SOCK_ACT_POLL_INT};
+echo "[SD_SOCK_ACT] Waiting for server on $sock to come up..."
+done;
+
+if [ ! -z $SD_SOCK_ACT_IDLE_TIMEOUT ]; then
+    SD_SOCK_ACT_SOCAT_FLAG="$SD_SOCK_ACT_SOCAT_FLAG -T$SD_SOCK_ACT_IDLE_TIMEOUT" 
+fi
+
+echo "[SD_SOCK_ACT] Start to forward connections from passed socket /dev/fd/$sock_fd to $sock"
+socat $SD_SOCK_ACT_SOCAT_FLAG ACCEPT-FD:$sock_fd,fork $sock &
+tunnel_pids+=$!
+
+sock_fd=$((sock_fd + 1))
+done
+
+wait "${tunnel_pids[@]}"
