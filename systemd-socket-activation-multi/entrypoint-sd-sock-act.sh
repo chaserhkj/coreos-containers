80ee4c4a7556bbb34adb023769cd268eae2c5ade
Chaser Huang <huangkangjing@gmail.com>
2025-09-12 18:00:07 -0400

    Change flag compose position

--- systemd-socket-activation-multi/entrypoint-sd-sock-act.sh
+++ systemd-socket-activation-multi/entrypoint-sd-sock-act.sh
@@ -14,16 +14,16 @@ trap "clean" EXIT
 tunnel_pids=""
 sock_fd=3
 
+if [ ! -z $SD_SOCK_ACT_IDLE_TIMEOUT ]; then
+    SD_SOCK_ACT_SOCAT_FLAG="$SD_SOCK_ACT_SOCAT_FLAG -T$SD_SOCK_ACT_IDLE_TIMEOUT" 
+fi
+
 for sock in $SD_SOCK_ACT_FWD_TARGETS; do
 
 until socat /dev/null $sock >/dev/null 2>&1
 do sleep ${SD_SOCK_ACT_POLL_INT};
 echo "[SD_SOCK_ACT] Waiting for server on $sock to come up..."
 done;
-
-if [ ! -z $SD_SOCK_ACT_IDLE_TIMEOUT ]; then
-    SD_SOCK_ACT_SOCAT_FLAG="$SD_SOCK_ACT_SOCAT_FLAG -T$SD_SOCK_ACT_IDLE_TIMEOUT" 
-fi
 
 echo "[SD_SOCK_ACT] Start to forward connections from passed socket /dev/fd/$sock_fd to $sock"
 socat $SD_SOCK_ACT_SOCAT_FLAG ACCEPT-FD:$sock_fd,fork $sock &
