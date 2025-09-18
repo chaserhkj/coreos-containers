ddb9ee7e48050f606dc810d4fe3e615e719facae
Chaser Huang <huangkangjing@gmail.com>
2025-09-18 06:38:56 -0400

    Add wait for pty device

--- systemd-socket-activation-stdio/entrypoint-sd-sock-act.sh
+++ systemd-socket-activation-stdio/entrypoint-sd-sock-act.sh
@@ -15,5 +15,10 @@ if [ ! -z $SD_SOCK_ACT_IDLE_TIMEOUT ]; then
     SD_SOCK_ACT_SOCAT_FLAG="$SD_SOCK_ACT_SOCAT_FLAG -T$SD_SOCK_ACT_IDLE_TIMEOUT" 
 fi
 
+until [ -e /tmp/service-pty ]; do
+    echo "[SD_SOCK_ACT] Waiting for pty to be created..."
+    sleep 1
+done
+
 echo "[SD_SOCK_ACT] Start to forward connections from passed socket /dev/fd/3 to STDIO through pty, max-children=1 is set"
 socat $SD_SOCK_ACT_SOCAT_FLAG ACCEPT-FD:3,fork,max-children=1 'FILE:/tmp/service-pty,rawer'
