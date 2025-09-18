8b8420f6d001e9385af7fd6a1bc567fec4217460
Chaser Huang <huangkangjing@gmail.com>
2025-09-17 21:48:43 -0400

    Add stdio variant to systemd-socket-activation layers

---
+++ systemd-socket-activation-stdio/entrypoint-sd-sock-act.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# Use sh for compatibility
+
+socat PTY,rawer,link=/tmp/service-pty EXEC:"${SD_SOCK_ACT_BASE_CMD}" &
+SRV_PID=$!
+
+clean() {
+    echo "[SD_SOCK_ACT] Shutting down service process..."
+    kill $SRV_PID
+    exit 0
+}
+trap "clean" EXIT
+
+if [ ! -z $SD_SOCK_ACT_IDLE_TIMEOUT ]; then
+    SD_SOCK_ACT_SOCAT_FLAG="$SD_SOCK_ACT_SOCAT_FLAG -T$SD_SOCK_ACT_IDLE_TIMEOUT" 
+fi
+
+echo "[SD_SOCK_ACT] Start to forward connections from passed socket /dev/fd/3 to STDIO through pty, max-children=1 is set"
+socat $SD_SOCK_ACT_SOCAT_FLAG ACCEPT-FD:3,fork,max-children=1 'FILE:/tmp/service-pty,rawer'
