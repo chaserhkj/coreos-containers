8f85f6a1835725f0cfb77f3d181074000b9c4379
Chaser Huang <huangkangjing@gmail.com>
2025-11-04 04:10:47 -0500

    Remove self-built wheels, migrating to prebuilt wheels pulled from pip

--- lib-build/radial-attention-wheel/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf vendor && mkdir vendor
-(cd vendor && git clone --depth=1 https://github.com/mit-han-lab/radial-attention)
-podman build . -t radial-attention-wheel
--- lib-build/sparse-sageattn-2-wheel/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf vendor && mkdir vendor
-(cd vendor && git clone --depth=1 https://github.com/Radioheading/Block-Sparse-SageAttention-2.0)
-podman build . -t sparse-sageattn-2-wheel
