6fe2d4c73c42214977184bc0727a269ce160f379
Chaser Huang <huangkangjing@gmail.com>
2026-01-10 02:42:39 -0500

    Install cuda into comfyui-runtime

---
+++ service/comfyui-runtime/install-cuda.sh
@@ -0,0 +1,7 @@
+[[ $TORCH_BACKEND == cu* ]] && exit 0
+set -e
+wget -O /tmp/keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i /tmp/keyring.deb
+rm -f /tmp/keyring.deb
+apt-get update
+apt-get install -y cuda-toolkit
