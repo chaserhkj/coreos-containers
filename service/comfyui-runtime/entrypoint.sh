03fe0a58bb43e2c92a1660ad18d5e01f7aaf1617
Chaser Huang <huangkangjing@gmail.com>
2026-01-10 01:53:10 -0500

    Add entrypoint script

---
+++ service/comfyui-runtime/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -eu
+
+CLI_ARGS="$@"
+CLI_ARGS+=" --listen 127.8.8.1 --port 8000"
+CLI_ARGS+=" --log-stdout"
+CLI_ARGS+=" --use-pytorch-cross-attention"
+
+if [[ $TORCH_BACKEND == rocm* ]]; then
+    # ROCM-specific optimizations
+    # These are architecture and device dependent, may need further tuning
+    # or some device-id-based guards here
+    export MIGRAPHX_MLIR_USE_SPECIFIC_OPS="attention"
+    export TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1
+    export PYTORCH_HIP_ALLOC_CONF=expandable_segments:True
+
+    export FLASH_ATTENTION_TRITON_AMD_ENABLE="TRUE"
+    export FLASH_ATTENTION_TRITON_AMD_AUTOTUNE="TRUE"
+
+    # Force use HIPBLASLT, since rocblas is giving a segfault
+    export MIOPEN_GEMM_ENFORCE_BACKEND=5
+    # Same thing for MIGraphX, this is related to ONNX
+    export MIGRAPHX_SET_GEMM_PROVIDER=hipblaslt
+    export MIOPEN_FIND_MODE=5
+
+    #export MIOPEN_LOG_LEVEL=5
+    #export MIOPEN_ENABLE_LOGGING=1
+    #export MIOPEN_ENABLE_LOGGING_CMD=1
+fi
+
+source /venv/bin/activate
+echo "[INFO] Prestart ENV exported"
+
+# Run custom install script, as provided by the mount point
+if [[ -f /app/install.sh ]]; then
+    bash /app/install.sh
+fi
+
+echo "[INFO] Running ComfyUI-Manager fix"
+if [[ ! -f /venv/COMFYUI_CUSTOM_NODES_FIXED ]]; then
+    python3 /app/ComfyUI/custom_nodes/ComfyUI-Manager/cm-cli.py fix all
+    touch /venv/COMFYUI_CUSTOM_NODES_FIXED
+fi
+echo "[INFO] Running ComfyUI-Manager update"
+python3 /app/ComfyUI/custom_nodes/ComfyUI-Manager/cm-cli.py update all
+
+echo "[INFO] Starting main ComfyUI process"
+exec python3 /app/ComfyUI/main.py $CLI_ARGS
