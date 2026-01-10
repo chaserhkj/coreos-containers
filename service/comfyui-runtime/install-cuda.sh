a6e6cc0fac36ec8cc743a8650623abf6290a1c08
Chaser Huang <huangkangjing@gmail.com>
2026-01-10 02:55:18 -0500

    Fix conditional logic

--- service/comfyui-runtime/install-cuda.sh
+++ service/comfyui-runtime/install-cuda.sh
@@ -1,4 +1,4 @@
-[[ $TORCH_BACKEND == cu* ]] && exit 0
+[[ $TORCH_BACKEND == cu* ]] || exit 0
 set -e
 wget -O /tmp/keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i /tmp/keyring.deb
